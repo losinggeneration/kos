ad7c1cdb07c46fb94bcad32268763ed707fe96fd
bard <bard@cdfb0c48-74c9-0310-bf96-eb916a5bdf39>
2004-01-02 06:14:24 +0000

    Change usleep() to use thd_sleep() instead of timer_spin_sleep() since thd_sleep() is now sensitive to threads being enabled or not.
    git-svn-id: https://cadcdev.svn.sourceforge.net/svnroot/cadcdev/kos@69 cdfb0c48-74c9-0310-bf96-eb916a5bdf39

--- libc/gen/usleep.c
+++ libc/gen/usleep.c
@@ -1,15 +1,15 @@
 /* KallistiOS ##version##
 
    usleep.c
-   (c)2001 Dan Potter
+   Copyright (C)2001,2004 Dan Potter
 
    $Id: usleep.c,v 1.1 2002/02/09 06:15:42 bardtx Exp $
 */
 
-#include <arch/timer.h>
+#include <kos/thread.h>
 
 /* usleep() */
 void usleep(unsigned long usec) {
-	timer_spin_sleep (usec / 1000);
+	thd_sleep (usec / 1000);
 }
 
