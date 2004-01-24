53bef3efc8f0002b38e23929c9b5b1a795eeaf49
bard <bard@cdfb0c48-74c9-0310-bf96-eb916a5bdf39>
2004-01-24 21:00:41 +0000

    Updated netcfg to be able to load settings from ISP flash
    git-svn-id: https://cadcdev.svn.sourceforge.net/svnroot/cadcdev/kos@103 cdfb0c48-74c9-0310-bf96-eb916a5bdf39

--- addons/include/kos/netcfg.h
+++ addons/include/kos/netcfg.h
@@ -17,15 +17,39 @@ __BEGIN_DECLS
 
 /* Network configuration info. This holds the information about how we
    will start the networking and what settings to use. */
+#define NETCFG_METHOD_DHCP	0
+#define NETCFG_METHOD_STATIC	1
+#define NETCFG_METHOD_PPPOE	4
+#define NETCFG_SRC_VMU		0
+#define NETCFG_SRC_FLASH	1
+#define NETCFG_SRC_CWD		2
+#define NETCFG_SRC_CDROOT	3
 typedef struct netcfg {
-	uint32	ip;
+	int	src;			// Config source
+	int	method;			// Configuration method
+	uint32	ip;			// IP addresses
 	uint32	gateway;
 	uint32	netmask;
-	char	driver[64];	// Program filename
+	uint32	broadcast;
+	uint32	dns[2];			// DNS servers
+	char	hostname[64];		// DHCP hostname
+	char	email[64];		// E-Mail address
+	char	smtp[64];		// SMTP server
+	char	pop3[64];		// POP3 server
+	char	pop3_login[64];		// POP3 login name
+	char	pop3_passwd[64];	// POP3 password
+	char	proxy_host[64];		// Proxy server hostname
+	int	proxy_port;		// Proxy server port
+	char	ppp_login[64];		// PPP login
+	char	ppp_passwd[64];		// PPP password
+	char	driver[64];		// Driver program filename (if any)
 } netcfg_t;
 
 // Load a network config from a specific file.
 int netcfg_load_from(const char * fn, netcfg_t * out);
+
+// Load a network config from the DC flash.
+int netcfg_load_flash(netcfg_t * out);
 
 // Load a network config. Tries the VMUs first, then the current
 // directory, and finally /cd.
--- addons/libkosutils/netcfg.c
+++ addons/libkosutils/netcfg.c
@@ -1,7 +1,7 @@
 /* KallistiOS ##version##
 
    netcfg.c
-   Copyright (C)2003 Dan Potter
+   Copyright (C)2003,2004 Dan Potter
 
 */
 
@@ -10,8 +10,13 @@
 #include <stdlib.h>
 #include <assert.h>
 #include <kos/netcfg.h>
+#include <dc/flashrom.h>
 
-CVSID("$Id: netcfg.c,v 1.1 2003/07/15 07:58:28 bardtx Exp $");
+/* This module attempts to ferret out a valid network configuration by
+   drawing on all available sources. A file stored on the VMU is tried
+   first, followed by reading the settings from the DC's flashrom. If
+   this fails, we try reading settings from the current dir in the VFS,
+   and then from the root of the CD. If that fails, we give up. */
 
 int netcfg_load_from(const char * fn, netcfg_t * out) {
 	FILE * f;
@@ -58,10 +63,91 @@ int netcfg_load_from(const char * fn, netcfg_t * out) {
 			out->gateway = strtoul(b, NULL, 16);
 		} else if (!strcmp(buf, "netmask")) {
 			out->netmask = strtoul(b, NULL, 16);
+		} else if (!strcmp(buf, "broadcast")) {
+			out->broadcast = strtoul(b, NULL, 16);
+		} else if (!strcmp(buf, "dns1")) {
+			out->dns[0] = strtoul(b, NULL, 16);
+		} else if (!strcmp(buf, "dns2")) {
+			out->dns[1] = strtoul(b, NULL, 16);
+		} else if (!strcmp(buf, "hostname")) {
+			strcpy(out->hostname, b);
+		} else if (!strcmp(buf, "email")) {
+			strcpy(out->email, b);
+		} else if (!strcmp(buf, "smtp")) {
+			strcpy(out->smtp, b);
+		} else if (!strcmp(buf, "pop3")) {
+			strcpy(out->pop3, b);
+		} else if (!strcmp(buf, "pop3_login")) {
+			strcpy(out->pop3_login, b);
+		} else if (!strcmp(buf, "pop3_passwd")) {
+			strcpy(out->pop3_passwd, b);
+		} else if (!strcmp(buf, "proxy_host")) {
+			strcpy(out->proxy_host, b);
+		} else if (!strcmp(buf, "proxy_port")) {
+			out->proxy_port = strtoul(b, NULL, 10);
+		} else if (!strcmp(buf, "ppp_login")) {
+			strcpy(out->ppp_login, b);
+		} else if (!strcmp(buf, "ppp_passwd")) {
+			strcpy(out->ppp_passwd, b);
+		} else if (!strcmp(buf, "method")) {
+			if (!strcmp(b, "dhcp"))
+				out->method = NETCFG_METHOD_DHCP;
+			else if (!strcmp(b, "static"))
+				out->method = NETCFG_METHOD_STATIC;
+			else if (!strcmp(b, "pppoe"))
+				out->method = NETCFG_METHOD_PPPOE;
 		}
 	}
 
 	fclose(f);
+
+	return 0;
+}
+
+int netcfg_load_flash(netcfg_t * out) {
+	flashrom_ispcfg_t cfg;
+
+	if (flashrom_get_ispcfg(&cfg) < 0)
+		return -1;
+
+	// Start out with a clean config
+	memset(out, 0, sizeof(netcfg_t));
+
+#define READIP(dst, src) \
+	dst = ((src[0]) << 24) | \
+		((src[1]) << 16) | \
+		((src[2]) << 8) | \
+		((src[3]) << 0)
+
+	if (cfg.ip_valid) {
+		READIP(out->ip, cfg.ip);
+		READIP(out->gateway, cfg.gw);
+		READIP(out->netmask, cfg.nm);
+		READIP(out->broadcast, cfg.bc);
+		READIP(out->dns[0], cfg.dns[0]);
+		READIP(out->dns[1], cfg.dns[1]);
+		strcpy(out->hostname, cfg.hostname);
+	}
+	if (cfg.email_valid)
+		strcpy(out->email, cfg.email);
+	if (cfg.smtp_valid)
+		strcpy(out->smtp, cfg.smtp);
+	if (cfg.pop3_valid)
+		strcpy(out->pop3, cfg.pop3);
+	if (cfg.pop3_login_valid)
+		strcpy(out->pop3_login, cfg.pop3_login);
+	if (cfg.pop3_passwd_valid) {
+		strcpy(out->pop3_passwd, cfg.pop3_passwd);
+		out->proxy_port = cfg.proxy_port;
+	}
+	if (cfg.ppp_login_valid) {
+		strcpy(out->proxy_host, cfg.proxy_host);
+		strcpy(out->ppp_login, cfg.ppp_login);
+	}
+	if (cfg.ppp_passwd_valid)
+		strcpy(out->ppp_passwd, cfg.ppp_passwd);
+
+#undef READIP
 
 	return 0;
 }
@@ -83,18 +169,33 @@ int netcfg_load(netcfg_t * out) {
 
 			sprintf(buf, "/vmu/%s/net.cfg", d->name);
 			if (netcfg_load_from(buf, out) >= 0) {
+				out->src = NETCFG_SRC_VMU;
 				fs_close(f);
 				return 0;
 			}
 		}
 	}
 
-	// Didn't work out.. try the current dir.
-	if (netcfg_load_from("net.cfg", out) >= 0)
+	// Couldn't find anything. Try reading the config
+	// from flash.
+	if (netcfg_load_flash(out) >= 0) {
+		out->src = NETCFG_SRC_FLASH;
 		return 0;
+	}
+
+	// Didn't work out->. try the current dir.
+	if (netcfg_load_from("net.cfg", out) >= 0) {
+		out->src = NETCFG_SRC_CWD;
+		return 0;
+	}
 
 	// Finally, try the CD
-	return netcfg_load_from("/cd/net.cfg", out);
+	if (netcfg_load_from("/cd/net.cfg", out) >= 0) {
+		out->src = NETCFG_SRC_CDROOT;
+		return 0;
+	}
+
+	return -1;
 }
 
 int netcfg_save_to(const char * fn, const netcfg_t * cfg) {
@@ -113,21 +214,40 @@ int netcfg_save_to(const char * fn, const netcfg_t * cfg) {
 	if (fwrite(buf, strlen(buf), 1, f) != 1)
 		goto error;
 
-	sprintf(buf, "driver=%s\n", cfg->driver);
-	if (fwrite(buf, strlen(buf), 1, f) != 1)
+#define WRITESTR(fmt, data) \
+	sprintf(buf, fmt, data); \
+	if (fwrite(buf, strlen(buf), 1, f) != 1) \
 		goto error;
 
-	sprintf(buf, "ip=%08lx\n", cfg->ip);
-	if (fwrite(buf, strlen(buf), 1, f) != 1)
-		goto error;
-
-	sprintf(buf, "gateway=%08lx\n", cfg->gateway);
-	if (fwrite(buf, strlen(buf), 1, f) != 1)
-		goto error;
-
-	sprintf(buf, "netmask=%08lx\n", cfg->netmask);
-	if (fwrite(buf, strlen(buf), 1, f) != 1)
-		goto error;
+	WRITESTR("driver=%s\n", cfg->driver);
+	WRITESTR("ip=%08lx\n", cfg->ip);
+	WRITESTR("gateway=%08lx\n", cfg->gateway);
+	WRITESTR("netmask=%08lx\n", cfg->netmask);
+	WRITESTR("broadcast=%08lx\n", cfg->broadcast);
+	WRITESTR("dns1=%08lx\n", cfg->dns[0]);
+	WRITESTR("dns2=%08lx\n", cfg->dns[1]);
+	WRITESTR("hostname=%s\n", cfg->hostname);
+	WRITESTR("email=%s\n", cfg->email);
+	WRITESTR("smtp=%s\n", cfg->smtp);
+	WRITESTR("pop3=%s\n", cfg->pop3);
+	WRITESTR("pop3_login=%s\n", cfg->pop3_login);
+	WRITESTR("pop3_passwd=%s\n", cfg->pop3_passwd);
+	WRITESTR("proxy_host=%s\n", cfg->proxy_host);
+	WRITESTR("proxy_port=%d\n", cfg->proxy_port);
+	WRITESTR("ppp_login=%s\n", cfg->ppp_login);
+	WRITESTR("ppp_passwd=%s\n", cfg->ppp_passwd);
+	switch (cfg->method) {
+	case 0:
+		WRITESTR("method=%s\n", "dhcp");
+		break;
+	case 1:
+		WRITESTR("method=%s\n", "static");
+		break;
+	case 4:
+		WRITESTR("method=%s\n", "pppoe");
+		break;
+	}
+#undef WRITESTR
 
 	fclose(f);
 
