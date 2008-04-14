0089384d7676f5e37aca947370bcd6d7c92cd87b
atani <atani@cdfb0c48-74c9-0310-bf96-eb916a5bdf39>
2008-04-14 16:10:23 +0000

    Upgrade to newlib 1.15.0
    git-svn-id: https://cadcdev.svn.sourceforge.net/svnroot/cadcdev/kos@565 cdfb0c48-74c9-0310-bf96-eb916a5bdf39

--- utils/dc-chain/download.sh
+++ utils/dc-chain/download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget -c ftp://ftp.gnu.org/gnu/binutils/binutils-2.17.tar.bz2 || exit 1
 wget -c ftp://ftp.gnu.org/gnu/gcc/gcc-3.4.6/gcc-3.4.6.tar.bz2 || exit 1
-wget -c ftp://sources.redhat.com/pub/newlib/newlib-1.12.0.tar.gz || exit 1
+wget -c ftp://sources.redhat.com/pub/newlib/newlib-1.15.0.tar.gz || exit 1
 
