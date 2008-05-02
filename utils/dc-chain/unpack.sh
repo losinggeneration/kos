97fab2458e57f5bb2cb4e443bef3d36c0fa44249
fackue <fackue@cdfb0c48-74c9-0310-bf96-eb916a5bdf39>
2008-05-02 16:06:18 +0000

    Newlib 1.15 not quite stable yet
    git-svn-id: https://cadcdev.svn.sourceforge.net/svnroot/cadcdev/kos@573 cdfb0c48-74c9-0310-bf96-eb916a5bdf39

--- utils/dc-chain/unpack.sh
+++ utils/dc-chain/unpack.sh
@@ -4,4 +4,4 @@ rm -rf binutils-2.17 gcc-3.4.6 newlib-1.15.0
 
 tar jxf binutils-2.17.tar.bz2 || exit 1
 tar jxf gcc-3.4.6.tar.bz2 || exit 1
-tar zxf newlib-1.15.0.tar.gz || exit 1
+tar zxf newlib-1.12.0.tar.gz || exit 1
